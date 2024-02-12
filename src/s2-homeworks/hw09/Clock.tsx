c6058ee584c034706eb9ee35f9aada72fc484445
listopadgeorgii <listopadiogl@gmail.com>
2024-02-12 22:11:03 +0300

    hw9Changes

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -39,8 +39,20 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = date.toLocaleTimeString() // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+    const stringTimeOptions = {
+        hour: '2-digit',
+        minute: '2-digit'
+    };
+
+
+    const stringDateOptions = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    }
+    const stringTime = date.toLocaleTimeString([], stringTimeOptions as any);// часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = date.toLocaleDateString([], stringDateOptions as any)  // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const options: {weekday: 'long'} = { weekday: 'long' };
